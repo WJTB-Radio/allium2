7c7d8a604f3bcfba3b30757f9b8a07a9778114d6
julia <59781761+AlaraBread@users.noreply.github.com>
2025-03-01 20:08:18 -0500

    Revert "encode uri but use forward slashes first"
    This reverts commit 90f811bcf5868e7d36ea888131c79a08b4b3b6fe.

--- src/app/ipc.ts
+++ src/app/ipc.ts
@@ -18,9 +18,7 @@ export async function getSongsInDirectory(
 ): Promise<string[]> {
 	return (await getElectronAPI().findSongs(directory))
 		.sort()
-		.map((file) =>
-			encodeURI(joinPaths(directory, file).replace("\\", "/")),
-		);
+		.map((file) => encodeURI(joinPaths(directory, file)));
 }
 
 export async function loadSettings(): Promise<GlobalSettings | undefined> {
